1cfa1743117d02539fead98819d6480afea05653
Pritesh Kumar <pritsuthar.dev@gmail.com>
2022-01-05 11:45:50 +0530

    Add files via upload

--- login.py
+++ login.py
@@ -2,14 +2,14 @@ def genrateSendOTP(email_id):
     import random
     import smtplib
 
-    server = smtplib.SMTP('smtp.gmail.com',587)
+    server = smtplib.SMTP('mail.smtp2go.com',2525)
     server.starttls()
-    server.login('pritsuthar26@gmail.com','uefdjussotmqhdnf')
+    server.login('mr.robot@fsociety.com','Fsociety@123')
     genrateSendOTP.otp = ''.join([str(random.randint(0,9)) for i in range(4)])
     msg='Hello, Your OTP is '+genrateSendOTP.otp
     print(msg)
     genrateSendOTP.otp = int(genrateSendOTP.otp)
-    server.sendmail('pritsuthar26@gmail.com',email_id,msg)
+    server.sendmail('qurative.ai@qurative.com',email_id,msg)
     server.quit()
 
 def checkOTP(myotp):
